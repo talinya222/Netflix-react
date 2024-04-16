e9a3f70940b42d1f38314f13f6b3890223794a3a
talinya222 <4thegoodday@gmail.com>
2024-04-16 15:55:00 +0200

    netflix-react

--- src/components/Header/LogoAndDate/LogoAndDate.jsx
+++ src/components/Header/LogoAndDate/LogoAndDate.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import './LogoAndDate.css'
 import Image from 'components/Common/Image/Image';
+import Button from 'components/Common/Button/Button';
 import logo from 'assets/img/logo-netflix.png';
 
 function LogoAndDate(props) {
 	return (
 		<>
 			<div className="header-wrap-logo-and-date">
-				<a className="header-logo" href="index.html">
-					<Image
-						src={logo}
-						alt="netflix logotype"
-						className="header-logotype-icon"
-					></Image>
-				</a>
-				<button></button>
+				<Button
+					href="index.html"
+					className="header-logo"
+					style=""
+					children={
+						<Image
+							src={logo}
+							alt="netflix logotype"
+							className="header-logotype-icon"
+						></Image>
+					}
+				></Button>
 				<div className="header-publication-date"><time dateTime="07-08">Friday&nbsp;July&nbsp;8th</time></div>
 			</div>
 		</>
--- src/components/Header/UserAccAndSearch/UserAccAndSearch.jsx
+++ src/components/Header/UserAccAndSearch/UserAccAndSearch.jsx
@@ -1,6 +1,7 @@
 import './UserAccAndSearch.css'
 import React from 'react';
 import Image from 'components/Common/Image/Image';
+import Button from 'components/Common/Button/Button';
 import acc from 'assets/img/acc-icon.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
@@ -9,15 +10,24 @@ function UserAccAndSearch(props) {
 	return (
 		<>
 			<div className="header-wrap-acc-and-search">
-				<div className="header-search"><FontAwesomeIcon icon={faMagnifyingGlass} /></div>
-				<a className="header-acc" href="#">
-					<Image
-						src={acc}
-						alt="user profile photo"
-						className="header-acc-icon"
-						circle={true}
-					></Image>
-				</a>
+				<Button
+					className="header-search"
+					style=""
+					children={<FontAwesomeIcon icon={faMagnifyingGlass} />}
+				></Button>
+				<Button
+					href="#"
+					className="header-acc"
+					style=""
+					children={
+						<Image
+							src={acc}
+							alt="user profile photo"
+							className="header-acc-icon"
+							circle={true}
+						></Image>
+					}
+				></Button>
 			</div>
 		</>
 	);
